531f6e8d854d41acb93a391b6022bb2af2316afa
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-12-31 23:05:22 +0100

    rxjs overview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+  // name: string | undefined;
+  // surname: string | undefined;
 
   formGroup: FormGroup | undefined;
+
+  observable$: Observable<number> = of(1,2,3,4,5);
+
+  subscription: Subscription | undefined;
+  formSubscription: Subscription | undefined;
+
 
   constructor(
     private formBuilder: FormBuilder
@@ -19,19 +29,37 @@ export class AppComponent implements OnInit {
   }
 
 
-
   ngOnInit(): void {
+    this.displayObservableNumbers();
     this.formGroup = this.formBuilder.group({
       name: [undefined, Validators.required],
       surname: [undefined, Validators.minLength(3)]
     })
+    this.observeFormChanges();
   }
 
-  // name: string | undefined;
-  // surname: string | undefined;
+
+  private displayObservableNumbers(): void {
+    this.subscription = this.observable$.subscribe((value) => {
+      console.log(value);
+    })
+  }
+
 
   onFormSubmit(): void {
     console.log(this.formGroup?.valid)
     console.log(this.formGroup?.value);
+  }
+
+  private observeFormChanges():void {
+    this.formSubscription = this.formGroup?.valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+  }
+
+  // don't forget to unsubscribe!!!
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+    this.formSubscription?.unsubscribe();
   }
 }
